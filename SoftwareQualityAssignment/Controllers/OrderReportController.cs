085d4caa1db354cc1cccd0c583aeb49c621c8dd8
Michael <Michael@DESKTOP-O3JBRD8>
2020-04-04 20:34:48 +0100

    Api for orders rewritten so that it returns the info needed for the orders report

---
+++ SoftwareQualityAssignment/Controllers/OrderReportController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SoftwareQualityAssignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace SoftwareQualityAssignment.Controllers
+{
+    [ApiController]
+    [Route("api/OrdersReport")]
+    public class OrderReportController : ControllerBase
+    {
+        private readonly Database _database;
+        public OrderReportController(Database database)
+        {
+            _database = database ?? throw new ArgumentNullException(nameof(database));
+        }
+        [HttpGet]
+        public IActionResult GetAllOrders()
+        {
+            IEnumerable<Order> Orders = _database.Orders.Include(x => x.OrderLines).ThenInclude(x => x.Product).Include(x => x.Customers)
+                .OrderBy(x => x.Customers)
+                .ToList();
+
+            //IEnumerable<Order> Orders = _database.Orders.Include(x => x.OrderLines).ThenInclude(x => x.Product).ToList();
+            return Ok(Orders);
+        }
+
+    }
+}
